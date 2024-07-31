828801c9955e292664dfa1d63c9763938d917795
ArmoredPony <raxmetov2003@gmail.com>
2024-08-01 00:17:51 +0400

    QoL method for `Team`

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,15 @@ pub enum Team {
   None,
 }
 
+impl Team {
+  pub fn is_opposing_team(&self, team: &Team) -> bool {
+    matches!(
+      (*self, *team),
+      (Team::White, Team::Black) | (Team::Black, Team::White)
+    )
+  }
+}
+
 impl Display for Team {
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
     write!(f, "{}", match self {
